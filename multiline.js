575b7c408893f37aed4d3a01fc59d007c5b02d50
xm <xingmengpan2013@gmail.com>
2019-03-12 12:30:46 +0800

    try to merge data source

---
+++ multiline.js
@@ -0,0 +1,147 @@
+function makeLineChart(dataset, xName, yObjs, axisLables) {
+    var chartObj = {};
+    var color = d3.scale.category10();
+    chartObj.xAxisLable = axisLables.xAxis;
+    chartObj.yAxisLable = axisLables.yAxis;
+    /*
+     yObjsects format:
+     {y1:{column:'',name:'name',color:'color'},y2}
+     */
+
+    chartObj.data = dataset;
+    chartObj.margin = {top: 15, right: 60, bottom: 30, left: 50};
+    chartObj.width = 650 - chartObj.margin.left - chartObj.margin.right;
+    chartObj.height = 480 - chartObj.margin.top - chartObj.margin.bottom;
+
+// So we can pass the x and y as strings when creating the function
+    chartObj.xFunct = function(d){return d[xName]};
+
+// For each yObjs argument, create a yFunction
+    function getYFn(column) {
+        return function (d) {
+            return d[column];
+        };
+    }
+
+// Object instead of array
+    chartObj.yFuncts = [];
+    for (var y  in yObjs) {
+        yObjs[y].name = y;
+        yObjs[y].yFunct = getYFn(yObjs[y].column); //Need this  list for the ymax function
+        chartObj.yFuncts.push(yObjs[y].yFunct);
+    }
+
+    //Formatter functions for the axes
+    chartObj.formatAsNumber = d3.format(".0f");
+    chartObj.formatAsDecimal = d3.format(".2f");
+    chartObj.formatAsCurrency = d3.format("$.2f");
+    chartObj.formatAsFloat = function (d) {
+        if (d % 1 !== 0) {
+            return d3.format(".2f")(d);
+        } else {
+            return d3.format(".0f")(d);
+        }
+
+    };
+
+    chartObj.xFormatter = chartObj.formatAsNumber;
+    chartObj.yFormatter = chartObj.formatAsFloat;
+
+    chartObj.bisectYear = d3.bisector(chartObj.xFunct).left; //< Can be overridden in definition
+
+//Create scale functions
+    chartObj.xScale = d3.scale.linear().range([0, chartObj.width]).domain(d3.extent(chartObj.data, chartObj.xFunct)); //< Can be overridden in definition
+
+// Get the max of every yFunct
+    chartObj.max = function (fn) {
+        return d3.max(chartObj.data, fn);
+    };
+    chartObj.yScale = d3.scale.linear().range([chartObj.height, 0]).domain([0, d3.max(chartObj.yFuncts.map(chartObj.max))]);
+
+    chartObj.formatAsYear = d3.format("");
+
+//Create axis
+    chartObj.xAxis = d3.svg.axis().scale(chartObj.xScale).orient("bottom").tickFormat(chartObj.xFormatter); //< Can be overridden in definition
+
+    chartObj.yAxis = d3.svg.axis().scale(chartObj.yScale).orient("left").tickFormat(chartObj.yFormatter); //< Can be overridden in definition
+// Build line building functions
+    function getYScaleFn(yObj) {
+        return function (d) {
+            return chartObj.yScale(yObjs[yObj].yFunct(d));
+        };
+    }
+    for (var yObj in yObjs) {
+        yObjs[yObj].line = d3.svg.line().interpolate("cardinal").x(function (d) {
+            return chartObj.xScale(chartObj.xFunct(d));
+        }).y(getYScaleFn(yObj));
+    }
+
+
+    chartObj.svg;
+
+// Change chart size according to window size
+    chartObj.update_svg_size = function () {
+        chartObj.width = parseInt(chartObj.chartDiv.style("width"), 10) - (chartObj.margin.left + chartObj.margin.right);
+
+        chartObj.height = parseInt(chartObj.chartDiv.style("height"), 10) - (chartObj.margin.top + chartObj.margin.bottom);
+
+        /* Update the range of the scale with new width/height */
+        chartObj.xScale.range([0, chartObj.width]);
+        chartObj.yScale.range([chartObj.height, 0]);
+
+        if (!chartObj.svg) {return false;}
+
+        /* Else Update the axis with the new scale */
+        chartObj.svg.select('.x.axis').attr("transform", "translate(0," + chartObj.height + ")").call(chartObj.xAxis);
+        chartObj.svg.select('.x.axis .label').attr("x", chartObj.width / 2);
+
+        chartObj.svg.select('.y.axis').call(chartObj.yAxis);
+        chartObj.svg.select('.y.axis .label').attr("x", -chartObj.height / 2);
+
+        /* Force D3 to recalculate and update the line */
+        for (var y  in yObjs) {
+            yObjs[y].path.attr("d", yObjs[y].line);
+        }
+
+
+        d3.selectAll(".focus.line").attr("y2", chartObj.height);
+
+        chartObj.chartDiv.select('svg').attr("width", chartObj.width + (chartObj.margin.left + chartObj.margin.right)).attr("height", chartObj.height + (chartObj.margin.top + chartObj.margin.bottom));
+
+        chartObj.svg.select(".overlay").attr("width", chartObj.width).attr("height", chartObj.height);
+        return chartObj;
+    };
+
+    chartObj.bind = function (selector) {
+        chartObj.mainDiv = d3.select(selector);
+        // Add all the divs to make it centered and responsive
+        chartObj.mainDiv.append("div").attr("class", "inner-wrapper").append("div").attr("class", "outer-box").append("div").attr("class", "inner-box");
+        chartSelector = selector + " .inner-box";
+        chartObj.chartDiv = d3.select(chartSelector);
+        d3.select(window).on('resize.' + chartSelector, chartObj.update_svg_size);
+        chartObj.update_svg_size();
+        return chartObj;
+    };
+
+// Render the chart
+    chartObj.render = function () {
+        //Create SVG element
+        chartObj.svg = chartObj.chartDiv.append("svg").attr("class", "chart-area").attr("width", chartObj.width + (chartObj.margin.left + chartObj.margin.right)).attr("height", chartObj.height + (chartObj.margin.top + chartObj.margin.bottom)).append("g").attr("transform", "translate(" + chartObj.margin.left + "," + chartObj.margin.top + ")");
+
+        // Draw Lines
+        for (var y  in yObjs) {
+            yObjs[y].path = chartObj.svg.append("path").datum(chartObj.data).attr("class", "line").attr("d", yObjs[y].line).style("stroke", color(y)).attr("data-series", y).on("mouseover", function () {
+                focus.style("display", null);
+            }).on("mouseout", function () {
+                focus.transition().delay(700).style("display", "none");
+            }).on("mousemove", mousemove);
+        }
+
+
+        // Draw Axis
+        chartObj.svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + chartObj.height + ")").call(chartObj.xAxis).append("text").attr("class", "label").attr("x", chartObj.width / 2).attr("y", 30).style("text-anchor", "middle").text(chartObj.xAxisLable);
+
+        chartObj.svg.append("g").attr("class", "y axis").call(chartObj.yAxis).append("text").attr("class", "label").attr("transform", "rotate(-90)").attr("y", -42).attr("x", -chartObj.height / 2).attr("dy", ".71em").style("text-anchor", "middle").text(chartObj.yAxisLable);
+
+   return chartObj;
+}
